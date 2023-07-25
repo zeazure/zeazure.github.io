cfd524391cc3fd84354633f42e5c5c8dbc90f6da
ZEAZURE <136143540+zeazure@users.noreply.github.com>
2023-07-25 22:00:43 +0700

    Add files via upload

---
+++ fitur/artikel/sejarah-lorem-ipsum/script.js
@@ -0,0 +1,9 @@
+const menu = document.getElementById('tema');
+const cont = document.getElementById('container');
+
+
+
+menu.addEventListener('click', function(){
+   cont.classList.toggle('color');
+    console.log('OK');
+}) 
